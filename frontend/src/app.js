3882779003a8c8092d5ac01af1cbdf02d27f8ec0
Kalyani-Bambal <kalyanibambal05@yahoo.com>
2025-12-26 07:32:27 +0000

    file update

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,15 +1,17 @@
-const API = "http://localhost:5000/api/products";
+const API = "http://BACKEND_SERVICE/api/products";
 
 function loadProducts() {
   fetch(API)
     .then(res => res.json())
     .then(data => {
-      document.getElementById("list").innerHTML = data.map(p =>
-        `<li>
-          ${p.name} - ₹${p.price}
-          <button onclick="deleteProduct(${p.id})">❌</button>
-        </li>`
-      ).join("");
+      list.innerHTML = "";
+      data.forEach(p => {
+        list.innerHTML += `
+          <li>
+            ${p.name} - ₹${p.price}
+            <button onclick="deleteProduct(${p.id})">❌</button>
+          </li>`;
+      });
     });
 }
 
@@ -30,4 +32,5 @@ function deleteProduct(id) {
 }
 
 loadProducts();
+
 
